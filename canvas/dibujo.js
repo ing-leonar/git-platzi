9bf8a85a6f033c5ba6cf007e80e4709d42dc0c28
ing-leonar <ing.leonar969@gmail.com>
2020-01-25 03:32:38 -0600

    version 1.0

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,29 +1,45 @@
+var texto= document.getElementById("texto_lineas");
+var boton= document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
 var d=document.getElementById("dibujito");
 var lienzo= d.getContext("2d");
-var lineas=30;
-var l=0;
-var yf, xi;
-var colorcito="#FAA";
 
-while (l<lineas)
+var ancho= d.width;
+var alto= d.height;
+var w=ancho/2;
+var h=alto/2;
+var l=0;
+
+
+function dibujoPorClick()
 {
-yf= 10*l;
-xi= 10*(l+1);
-dibujarLinea("grey", 0, yf, xi, 300);
-console.log("Linea"+ l);
-l=l+2;
+    var lineas= parseInt(texto.value);
+    
+    
+    for (l=0; l<lineas; l++)
+{   
+var colorcito="#black"; 
+var espaciox= w/lineas;
+var espacioy= h/lineas;
+var increm_0x= espaciox+l;
+var increm_1x= espaciox+(l+1);
+var increm_0y= espacioy+l;
+var increm_1y= espacioy+(l+1);
+
+yi= h-increm_0y;
+xf= increm_1x;
+dibujarLinea(colorcito, 0, yi, xf, 0);
+xi= w+increm_0x;
+yf= increm_1y;
+dibujarLinea(colorcito, xi, 0, ancho, yf);
+yi= h+increm_0y;
+xf= ancho-increm_1x;
+dibujarLinea(colorcito, ancho, yi, xf, alto);
+xi= w-increm_0x;
+yf= alto-increm_1y;
+dibujarLinea(colorcito, xi, alto, 0, yf);
+
+console.log("Linea" + l);
 
 }
-dibujarLinea(colorcito, 299,1,1,1);
-dibujarLinea(colorcito, 299,299,299,1);
-
-function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
-{
-    lienzo.beginPath();
-    lienzo.strokeStyle=color;
-    lienzo.moveTo(yinicial,xinicial);
-    lienzo.lineTo(yfinal,xfinal);
-    lienzo.stroke();
-    lienzo.closePath();
-    
 }
